0d7af45fc6f85c614112d7860c7ae9668b398e03
yinxueyuan <838476901@qq.com>
2017-08-25 22:40:28 +0800

    feat: image

--- methods/web.go
+++ methods/web.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"net/http"
 	"time"
 )
@@ -34,7 +33,4 @@ func main() {
 	if err != nil {
 		fmt.Printf("Server failed: ", err.Error())
 	}
-	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	fmt.Println(m.Bounds())
-	fmt.Println(m.At(0, 0).RGBA())
 }
